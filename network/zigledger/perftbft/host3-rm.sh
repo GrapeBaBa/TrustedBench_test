b9b5d0436d52395f6ec389de6fbcb1d906834686
grapebaba <281165273@qq.com>
2018-09-05 21:26:43 +0800

    Update perf test
    Signed-off-by: grapebaba <281165273@qq.com>

--- network/zigledger/perftbft/host3-rm.sh
+++ network/zigledger/perftbft/host3-rm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-rm -rf network/zigledger/bftnetwork/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tendermint/mychannel/*;
-rm -rf network/zigledger/bftnetwork/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tendermint/testchainid/*;
+rm -rf crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tendermint/mychannel/*;
+rm -rf crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tendermint/testchainid/*;
